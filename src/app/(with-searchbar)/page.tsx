21429d189878c7086fe989d693014e73b1e9315e
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-11 21:17:44 +0900

    index 페이지 데이터 페칭 구현

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,44 @@
 import MovieItem from "@/app/components/movie-item";
 import style from "./page.module.css";
 import movies from "@/dummy.json";
+import { MovieData } from "@/types";
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 60 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const movies: MovieData[] = await response.json();
+
+  return (
+    <div>
+      {movies.slice(0, 3).map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const movies: MovieData[] = await response.json();
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -8,19 +46,11 @@ export default function Home() {
       <div className={style.container}>
         <section className={style.recommend_section}>
           <h3>지금 가장 추천하는 영화</h3>
-          <div>
-            {movies.slice(0, 3).map((movie) => (
-              <MovieItem key={movie.id} {...movie} />
-            ))}
-          </div>
+          <RecoMovies />
         </section>
         <section className={style.all_section}>
           <h3>등록된 모든 영화</h3>
-          <div>
-            {movies.map((movie) => (
-              <MovieItem key={movie.id} {...movie} />
-            ))}
-          </div>
+          <AllMovies />
         </section>
       </div>
     </>
