263e53f151b6d376be47055ac9c36c9e38db9c6e
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-11 16:07:36 +0900

    서치바 컴포넌트 생성

---
+++ src/app/(with-searchbar)/searchbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function Searchbar() {
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+  const router = useRouter();
+  const [search, setSearch] = useState(q || "");
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (search.trim() && q !== search) {
+      router.push(`/search?q=${search}`);
+    }
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+        placeholder="검색어를 입력하세요 ..."
+      />
+      <button onSubmit={onSubmit}>검색</button>
+    </div>
+  );
+}
