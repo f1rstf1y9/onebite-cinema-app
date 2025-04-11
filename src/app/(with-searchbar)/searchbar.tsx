15dac9652b56733602eb505395617c690270ba75
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-11 17:48:37 +0900

    서치바 컴포넌트 스타일링 적용

--- src/app/(with-searchbar)/searchbar.tsx
+++ src/app/(with-searchbar)/searchbar.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import style from "./searchbar.module.css";
 
 export default function Searchbar() {
   const searchParams = useSearchParams();
@@ -30,7 +31,7 @@ export default function Searchbar() {
   };
 
   return (
-    <div>
+    <div className={style.searchbar_container}>
       <input
         value={search}
         onChange={onChangeSearch}
