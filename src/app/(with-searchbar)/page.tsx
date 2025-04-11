bd218cc3e4c22b57c50d880a890bc3a84a0d4b4a
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-11 17:55:02 +0900

    index 페이지 스타일 적용

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import MovieItem from "@/app/components/movie-item";
+import style from "./page.module.css";
+import movies from "@/dummy.json";
 
 export default function Home() {
-  return <div className={styles.page}>Index</div>;
+  return (
+    <>
+      <div className={style.container}>
+        <section className={style.recommend_section}>
+          <h3>지금 가장 추천하는 영화</h3>
+          <div>
+            {movies.slice(0, 3).map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+        <section className={style.all_section}>
+          <h3>등록된 모든 영화</h3>
+          <div>
+            {movies.map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
