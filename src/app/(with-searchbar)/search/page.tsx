28013726435401fe8b7483dd11a91f10a1663b08
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-12 18:33:03 +0900

    search 페이지에 Suspense 및 스켈레톤 UI 적용

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,15 +1,11 @@
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
 import style from "./page.module.css";
 import MovieItem from "@/components/movie-item";
 import { MovieData } from "@/types";
 import { delay } from "@/utils/delay";
+import { Suspense } from "react";
 
-export default async function page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
-  const { q } = await searchParams;
-
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
@@ -19,12 +15,27 @@ export default async function page({
     return <div>오류가 발생했습니다...</div>;
   }
   const movies: MovieData[] = await response.json();
-
   return (
-    <div className={style.container}>
+    <div>
       {movies.map((movie) => (
         <MovieItem key={movie.id} {...movie} />
       ))}
+    </div>
+  );
+}
+
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <div className={style.container}>
+      <Suspense key={q || ""} fallback={<MovieListSkeleton count={3} />}>
+        <SearchResult q={q || ""} />
+      </Suspense>
     </div>
   );
 }
