ce7679c67c24bd8dcbd1b72497f38d265529af2c
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-12 16:32:39 +0900

    search 페이지 딜레이 적용

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import style from "./page.module.css";
 import MovieItem from "@/app/components/movie-item";
 import { MovieData } from "@/types";
+import { delay } from "@/utils/delay";
 
 export default async function page({
   searchParams,
@@ -8,6 +9,8 @@ export default async function page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
+
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
     { cache: "force-cache" }
