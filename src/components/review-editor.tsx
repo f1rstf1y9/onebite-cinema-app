76cc6b5065080432507be275e4232472bf2b40e7
f1rstf1y9 <f1rstf1y9@naver.com>
2025-04-23 23:06:20 +0900

    ReviewEditor 스타일링 적용

--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,12 +1,13 @@
+import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review.action";
 
 export default function ReviewEditor({ movieId }: { movieId: string }) {
   return (
     <section>
-      <form action={createReviewAction} className="form_container">
+      <form action={createReviewAction} className={style.form_container}>
         <input type="text" name="movieId" value={movieId} hidden readOnly />
         <textarea name="content" placeholder="리뷰 내용" />
-        <div>
+        <div className={style.submit_container}>
           <input type="text" name="author" placeholder="작성자" />
           <button>작성하기</button>
         </div>
